953b42171a7fbe5bf89c90300d866cac25b12d60
mustapha <m.touil.mt@gmail.com>
2020-06-05 14:51:49 +0200

    creation d un jeu de donnée pour LivreApplication lors du lancement de l'application

--- livre/src/main/java/com/ocr/livre/LivreApplication.java
+++ livre/src/main/java/com/ocr/livre/LivreApplication.java
@@ -20,7 +20,25 @@ private LivreDao livreDao;
 
 	@PostConstruct
 	public void postConstruct(){
-		Livre livre1 = new Livre("WRIGHT","Richard","Black Boy","GALLIMARD","https://m.media-amazon.com/images/I/51JtLYQSchL.jpg" );
+		Livre livre1 = new Livre("WRIGHT","Richard","Black Boy","GALLIMARD","https://images-na.ssl-images-amazon.com/images/I/41sH5979BrL._SX302_BO1,204,203,200_.jpg" );
 		livreDao.save(livre1);
+		Livre livre2 = new Livre("WRIGHT","Richard","Black Boy","GALLIMARD","https://images-na.ssl-images-amazon.com/images/I/41sH5979BrL._SX302_BO1,204,203,200_.jpg" );
+		livreDao.save(livre2);
+		Livre livre3 = new Livre("WRIGHT","Richard","Black Boy","GALLIMARD","https://images-na.ssl-images-amazon.com/images/I/41sH5979BrL._SX302_BO1,204,203,200_.jpg" );
+		livreDao.save(livre3);
+		Livre livre4 = new Livre("STEINBECK","JOHN","Les Raisins De La Colere","GALLIMARD","https://images-na.ssl-images-amazon.com/images/I/418A1zRYhGL._SX302_BO1,204,203,200_.jpg" );
+		livreDao.save(livre4);
+		Livre livre5 = new Livre("STEINBECK","JOHN","Les Raisins De La Colere","GALLIMARD","https://images-na.ssl-images-amazon.com/images/I/418A1zRYhGL._SX302_BO1,204,203,200_.jpg" );
+		livreDao.save(livre5);
+		Livre livre6 = new Livre("STEINBECK","JOHN","Les Raisins De La Colere","GALLIMARD","https://images-na.ssl-images-amazon.com/images/I/418A1zRYhGL._SX302_BO1,204,203,200_.jpg" );
+		livreDao.save(livre6);
+		Livre livre7 = new Livre("LEE","Harper","Ne Tirez Pas Sur L'Oiseau Moqueur","LE LIVRE DE POCHE","https://m.media-amazon.com/images/I/41mY4e0kS9L.jpg");
+		livreDao.save(livre7);
+		Livre livre8 = new Livre("LEE","Harper","Ne Tirez Pas Sur L'Oiseau Moqueur","LE LIVRE DE POCHE","https://m.media-amazon.com/images/I/41mY4e0kS9L.jpg");
+		livreDao.save(livre8);
+		Livre livre9 = new Livre("LEE","Harper","Ne Tirez Pas Sur L'Oiseau Moqueur","LE LIVRE DE POCHE","https://m.media-amazon.com/images/I/41mY4e0kS9L.jpg");
+		livreDao.save(livre9);
+
+
 	}
 }
