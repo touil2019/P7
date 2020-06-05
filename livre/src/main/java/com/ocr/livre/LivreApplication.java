68cd203914ba44e64308ef4fc02b73047771a53c
mustapha <m.touil.mt@gmail.com>
2020-06-05 16:34:22 +0200

    ajout de la config client eureka lancement et affichage de l instance microservice livre avec UP en annotation

--- livre/src/main/java/com/ocr/livre/LivreApplication.java
+++ livre/src/main/java/com/ocr/livre/LivreApplication.java
@@ -5,10 +5,12 @@ import com.ocr.livre.model.Livre;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LivreApplication {
 
 @Autowired
